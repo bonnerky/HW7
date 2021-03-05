58bdfb0e7f6da055ff473522514655dac2b6c892
bonnerky <kylebonner99@gmail.com>
2021-03-04 21:57:04 -0800

    added test 3, if 4 inputed, should say it is a leap year

--- leapyear_test.py
+++ leapyear_test.py
@@ -5,6 +5,7 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear.leapyear(-1), "error")
         self.assertEqual(leapyear.leapyear(5), "Year is not a leap year")
+        self.assertEqual(leapyear.leapyear(4), "This is a leap year!")        
         
     
 if __name__ == '__main__':
