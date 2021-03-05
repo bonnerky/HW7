0d051866a089c507720ea938634e7e1d046e21e3
bonnerky <kylebonner99@gmail.com>
2021-03-04 21:53:13 -0800

    added test 2, checks if year is divisible by 4

--- leapyear_test.py
+++ leapyear_test.py
@@ -4,7 +4,7 @@ import leapyear
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear.leapyear(-1), "error")
-        
+        self.assertEqual(leapyear.leapyear(5), "Year is not a leap year")
         
     
 if __name__ == '__main__':
