ffb8296c03fcfab66bc29ed018e53032798050e8
bonnerky <kylebonner99@gmail.com>
2021-03-04 21:40:28 -0800

    answered test 3, outputs buzz when a%5 == 0

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,8 @@ def fizzbuzz (a):
     if a%3 == 0:
         s = str(a) + " fizz"
         return s
+    elif a%5 == 0:
+        s = str(a) + " buzz"
+        return s
     else:
         return str(a)
