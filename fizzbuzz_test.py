4852740f5c8cf4c113162d8e440e9d96dd33eb47
bonnerky <kylebonner99@gmail.com>
2021-03-04 21:38:49 -0800

    added test 3, inputs 5 and requests "5 buzz

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,6 +5,7 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(fizzbuzz.fizzbuzz(32), "32")
         self.assertEqual(fizzbuzz.fizzbuzz(3), "3 fizz")
+        self.assertEqual(fizzbuzz.fizzbuzz(5), "5 buzz")
         
         
     
