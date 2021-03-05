2bd34a903892e05cc6e6b0cdeafdca900ae1901c
bonnerky <kylebonner99@gmail.com>
2021-03-04 21:59:52 -0800

    added test 4. checks if year %400. if no returns not a leap year

--- leapyear_test.py
+++ leapyear_test.py
@@ -6,7 +6,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(leapyear.leapyear(-1), "error")
         self.assertEqual(leapyear.leapyear(5), "Year is not a leap year")
         self.assertEqual(leapyear.leapyear(4), "This is a leap year!")        
-        
+        self.assertEqual(leapyear.leapyear(100), "Year is not a leap year") 
     
 if __name__ == '__main__':
     unittest.main()
