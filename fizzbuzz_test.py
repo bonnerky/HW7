aa50f565416a93c726bc5bb99b54b152d0ec1b0a
bonnerky <kylebonner99@gmail.com>
2021-03-04 21:35:06 -0800

    added test 2, inputs 3 and requests "3 fizz"

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -4,6 +4,9 @@ import fizzbuzz
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(fizzbuzz.fizzbuzz(32), "32")
+        self.assertEqual(fizzbuzz.fizzbuzz(3), "3 fizz")
+        
+        
     
 if __name__ == '__main__':
     unittest.main()
